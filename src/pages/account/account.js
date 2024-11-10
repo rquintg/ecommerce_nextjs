ad10402a16277697aa148ee5fe8153c68e1fb81b
Ricardo Q. <ricardoqg41@icloud.com>
2024-11-10 18:48:28 -0500

    add tabsOptions

--- src/pages/account/account.js
+++ src/pages/account/account.js
@@ -1,11 +1,71 @@
+import {Tab} from 'semantic-ui-react';
+import {useRouter} from 'next/router';
 import {BasicLayout} from "@/layouts";
+import {useAuth} from '@/Hooks'
 import {Info} from "@/components/Account";
 
+import styles from './account.module.scss';
+
 export default function AccountPage() {
+
+    const {user, logout} = useAuth();
+    const router = useRouter();
+
+    if(!user){
+        router.push('/');
+        return null;
+    }
+
+    const panes = [
+        {
+            menuItem: 'Mis pedidos',
+            render: () => (
+                <Tab.Pane attached={false}>
+                    <p>Mis pedidos...</p>
+                </Tab.Pane>
+            )
+        },
+        {
+            menuItem: 'Lista de deseos',
+            render: () => (
+                <Tab.Pane attached={false}>
+                    <p>Lista de desos...</p>
+                </Tab.Pane>
+            )
+        },
+        {
+            menuItem: 'Direcciones',
+            render: () => (
+                <Tab.Pane attached={false}>
+                    <p>Mis direcciones...</p>
+                </Tab.Pane>
+            )
+        },
+        {
+            menuItem: {
+                icon: 'settings',
+                content: 'Ajustes',
+            },
+            render: () => (
+                <Tab.Pane attached={false}>
+                    <p>Mis ajustes...</p>
+                </Tab.Pane>
+            )
+        },
+        {
+            menuItem: {
+                icon: 'logout',
+                content: '',
+                onClick: logout,
+            },
+        },
+    ]
+
   return (
     <>
         <BasicLayout isContainer relative>
            <Info/>
+            <Tab menu={{ secondary: true, pointing: true }} panes={panes} className={styles.tabs} />
         </BasicLayout>
     </>
   );
