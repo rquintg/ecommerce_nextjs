c4744db6f08ac19a84629c1b787a4193dd1024c7
Ricardo Q. <ricardoqg41@icloud.com>
2024-11-20 17:54:58 -0500

    add list address

---
+++ src/components/Account/Address/ListAddresses/ListAddresses.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import {map} from 'lodash';
+import {Address as AddressCtrl } from '@/api';
+import {useAuth} from '@/Hooks';
+import {Address} from './Address';
+
+import styles from './ListAddresses.module.scss';
+
+const addressCtrl = new AddressCtrl();
+
+export function ListAddresses (){
+
+    const [addresses, setAddresses] = useState([]);
+    const {user} = useAuth();
+
+    useEffect(() => {
+        (async ()=> {
+            try {
+                const response = await addressCtrl.getAll(user.id);
+                setAddresses(response.data);
+            } catch (error) {
+                console.error('Error al obtener las direcciones:', error);
+            }
+        })()
+
+    }, []);
+
+    if(!addresses.length){
+        return (
+            <div>
+                <h2>No tienes direcciones registradas...</h2>
+            </div>
+        )
+    }
+
+    return (
+        <div className={styles.addresses}>
+            {map(addresses, (address) => (
+                <Address key={address.id}
+                         addressId={address.id}
+                         address={address.attributes}
+                />
+            ))}
+        </div>
+    )
+}
