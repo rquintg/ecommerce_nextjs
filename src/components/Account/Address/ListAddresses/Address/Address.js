a0f074dc5a5f5fb23bf47db8a03f172951337784
Ricardo Q. <ricardoqg41@icloud.com>
2024-11-25 02:16:39 -0500

    apply, update and delete address

--- src/components/Account/Address/ListAddresses/Address/Address.js
+++ src/components/Account/Address/ListAddresses/Address/Address.js
@@ -1,8 +1,31 @@
+import {useState} from "react";
 import {Button, Icon} from "semantic-ui-react";
+import {BasicModal, Confirm} from "@/components/Shared";
+import {Address as AddressCtrl} from "@/api";
+import {AddressForm} from '../../AddressForm';
 import styles from './Address.module.scss';
+
+
+const addressCtrl = new AddressCtrl();
 
 export function Address (props){
     const {addressId, address, onReload} = props;
+
+    const [showEdit, setShowEdit] = useState(false);
+    const [showConfirm, setShowConfirm] = useState(false);
+
+    const openCloseEdit = () => setShowEdit((prevState) => !prevState);
+    const openCloseConfirm = () => setShowConfirm((prevState) => !prevState);
+
+
+    const onDelete = async () => {
+        try {
+            await addressCtrl.delete(addressId);
+            onReload();
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return(
         <>
@@ -16,15 +39,32 @@ export function Address (props){
                 </div>
 
                 <div className={styles.actions}>
-                    <Button primary icon>
+                    <Button primary icon onClick={openCloseEdit}>
                         <Icon name="pencil"/>
                     </Button>
-                    <Button primary icon>
+                    <Button primary icon onClick={openCloseConfirm}>
                         <Icon name="delete"/>
                     </Button>
                 </div>
-
             </div>
+
+            <Confirm
+                open={showConfirm}
+                onCancel={openCloseConfirm}
+                onConfirm={onDelete}
+                content={`¿Estás seguro de eliminar la dirección ${address.title}?`}
+            />
+
+            <BasicModal show={showEdit}
+                        onClose={openCloseEdit}
+                        title="Editar dirección"
+            >
+                <AddressForm onClose={openCloseEdit}
+                             onReload={onReload}
+                             addressId={addressId}
+                             address={address}
+                />
+            </BasicModal>
         </>
     )
 }
