e89da25b37f6ed3ffffc069ed32abc11fa561ea1
Ricardo Q. <ricardoqg41@icloud.com>
2024-11-25 04:01:15 -0500

    show data BannerLastGamePublished

--- src/components/Home/BannerLastGamePublished/BannerLastGamePublished.js
+++ src/components/Home/BannerLastGamePublished/BannerLastGamePublished.js
@@ -1,4 +1,7 @@
 import {useEffect, useState} from "react";
+import {Container, Image} from "semantic-ui-react";
+import {DateTime} from "luxon";
+import Link from "next/link";
 import {Game} from "@/api";
 import styles from './BannerLastGamePublished.module.scss';
 
@@ -21,9 +24,28 @@ const gameCtrl = new Game();
 
     if(!game) return null;
 
+    const wallpaper = game.attributes.wallpaper;
+    const releaseDate = new Date(game.attributes.releaseDate).toISOString();
     return (
-        <div>
-            <h2>BannerLastGamePublished</h2>
+        <div className={styles.container}>
+            <Image src={wallpaper.data.attributes.url} className={styles.wallpaper} />
+
+            <Link className={styles.infoContainer} href={game.attributes.slug}>
+                <Container>
+                    <span className={styles.date}>
+                        {DateTime.fromISO(releaseDate).minus({days: 1}).toRelative()}
+                    </span>
+
+                    <h2>
+                        {game.attributes.title}
+                    </h2>
+
+                    <p className={styles.price}>
+                        <label>-40%</label>
+                        <span className={styles.finalPrice}>$59.99</span>
+                    </p>
+                </Container>
+            </Link>
         </div>
     );
 }
